877ed245903ebefc5958d1bb281a8ad156c7cb30
nicole <nicolekirimi123@gmail.com>
2024-05-22 11:52:54 +0300

    Updates

---
+++ 1c822617baeebac7abb2.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const orderButton = document.getElementById("orderButton");
+
+    orderButton.addEventListener("click", () => {
+        const caPackets = document.getElementById("ca-packets").value;
+        const coPackets = document.getElementById("co-packets").value;
+        const dPackets = document.getElementById("d-packets").value;
+        const mPackets = document.getElementById("m-packets").value;
+
+        const caPrice = 1500;
+        const coPrice = 400;
+        const dPrice = 500;
+        const mPrice = 600;
+
+        const caTotal = caPackets ? parseInt(caPackets) * caPrice : 0;
+        const coTotal = coPackets ? parseInt(coPackets) * coPrice : 0;
+        const dTotal = mPackets ? parseInt(dPackets) * dPrice : 0;
+        const mTotal = mPackets ? parseInt(mPackets) * mPrice : 0;
+
+        const totalAmount = caTotal + coTotal + dTotal + mTotal;
+
+        alert(`Order successful! Total amount: $${totalAmount.toFixed(2)}`);
+    });
+});
